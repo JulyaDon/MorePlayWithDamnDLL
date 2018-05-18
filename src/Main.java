dc0c19e25e646334238f34dceb664eee5afe2998
JulyaDon <don.julya@yandex.ru>
2018-05-18 16:57:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by July on 18.05.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyLib myLib = new MyLib();
+        System.out.println(myLib.mainFunc(6));
+    }
+}
---
+++ src/MyLib.java
@@ -0,0 +1,17 @@
+/**
+ * Created by July on 18.05.2018.
+ */
+public class MyLib {
+    static {
+        try{
+            /* MyJNI.dll (Windows) or libMyJNI.so (Linux) */
+            System.loadLibrary("Test_Of_DLL");
+        } catch(java.lang.UnsatisfiedLinkError err){
+            /* Error output when native binary not present */
+            System.out.println("Unable to load MyJNI library");
+            System.exit(0);
+        }
+    }
+
+    public native String mainFunc (int n);
+}
